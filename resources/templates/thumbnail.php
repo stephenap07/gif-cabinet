fb2037b9eb9e04357daea7393a1b668904017ac0
Jason Cummings <jsncmgs1@gmail.com>
2015-04-19 20:17:54 -0400

    Move header into partial, some style tweaks

--- resources/templates/thumbnail.php
+++ resources/templates/thumbnail.php
@@ -3,7 +3,9 @@
 		$tag = $tags[array_rand($tags)];
 ?>
 <div class="img-responsive img-thumbnail col-sm-12 col-md-4 <?php echo($tag);?>">
-	<img class='gif-image' src=<?php echo $config['paths']['resources'] . '/images/1407801019670.gif' ?>>
+	<a href='detailed_view.php'>
+		<img class='gif-image' src=<?php echo $config['paths']['resources'] . '/images/1407801019670.gif' ?>>
+	</a>
 	<div class="caption">
 		<h3> GIF Label  <small>&bull; <?php 
 		// for testing filter... needs to be dynamic
