9ff91f326a8cd9930a71463b4deef69f26f70adb
Jason Cummings <jsncmgs1@gmail.com>
2015-04-19 21:03:06 -0400

    Add preview to gif upload

--- resources/templates/form_upload.php
+++ resources/templates/form_upload.php
@@ -1,12 +1,27 @@
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<form action="upload.php" id="gif-form" method="post" enctype="multipart/form-data">
 	<div class="form-group">
-	    <label for="fileToUpload">Select image to upload:</label>
-	    <input type="file" name="fileToUpload" id="fileToUpload">
-	    <p class='help-block'>Upload an image that shows the issue happening</p>
+		<label for="fileToUpload">Select image to upload:</label>
+		<img id="upload-preview" style="width: 100px; height: 100px; display: none;" />
+		<input id="upload-image" type="file" name="myPhoto" onchange="PreviewImage();" />
+		<script type="text/javascript">
+
+			function PreviewImage() {
+				var oFReader = new FileReader();
+				oFReader.readAsDataURL(document.getElementById("upload-image").files[0]);
+
+				oFReader.onload = function (oFREvent) {
+					$preview = $('#upload-preview')[0];
+					$preview.src = oFREvent.target.result;
+					$($preview).css('display', 'initial');
+				};
+			};
+
+		</script>
+		<p class='help-block'>Upload an image that shows the issue happening</p>
 	</div>
 	<div class="form-group">
-    <label for="issue-upload">What is the issue?</label>
-    <textarea class='form-control' name='issue-upload' id='issue-upload'></textarea>
-  </div> 
+		<label for="issue-upload">What is the issue?</label>
+		<textarea class='form-control' name='issue-upload' id='issue-upload'></textarea>
+	</div> 
 	<button class="btn btn-primary" type="submit" name="submit">Submit</button>
 </form>
